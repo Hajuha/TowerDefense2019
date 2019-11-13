68b3cec223de27896caeed2f48eab6ae2ddc5620
Hao Hoang <hajuha168@gmail.com>
2019-11-14 02:50:13 +0700

    cleaner code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +14,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         GameStage  gameStage = new GameStage();
         primaryStage = gameStage.getMainStage();
-        primaryStage.show();
+        Stage root = new Stage();
+        Button startBtn = new Button("Start");
+        VBox layout = new VBox();
+        Stage finalPrimaryStage = primaryStage;
+        startBtn.setOnAction(e -> finalPrimaryStage.show());
+        layout.getChildren().addAll(startBtn);
+        Scene startScene = new Scene(layout, 1200, 700);
+        root.setScene(startScene);
+        root.show();
     }
 
 
